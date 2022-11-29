f1f5a684850c0ab330785d4412b8c5f637a09e23
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2022-11-29 16:49:22 +0300

    ss

--- Assets/LedgeGrab.cs
+++ Assets/LedgeGrab.cs
@@ -12,26 +12,41 @@ public class LedgeGrab : MonoBehaviour
     [SerializeField] LayerMask ledge;
     private BoxCollider2D col;
     private Animator anim;
-
+    private SpriteRenderer sr;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<BoxCollider2D>();
         startingGrav = rb.gravityScale;  //the initially gravity is stored in the array
         anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        greenBox = Physics2D.OverlapBox(new Vector2(transform.position.x + (greenXOffset * transform.localScale.x), transform.position.y + greenYOffset), new Vector2(greenXsize, greenYSize),   //creates a box!
-            0, ledge);
-        //we dont need GreenYOffset* transform.localscale.y because the Y axis is fixed when rotating on X.axis, but we do need it for the X axis
+        if(sr.flipX)
+        {
+            greenXOffset = -.1f;
+            redXOffset = -0.06f;
+            greenBox = Physics2D.OverlapBox(new Vector2(transform.position.x + (greenXOffset * transform.localScale.x), transform.position.y + greenYOffset), new Vector2(greenXsize, greenYSize), 0f, ledge);
+            //we dont need GreenYOffset* transform.localscale.y because the Y axis is fixed when rotating on X.axis, but we do need it for the X axis
 
-        RedBox = Physics2D.OverlapBox(new Vector2(transform.position.x + (redXOffset * transform.localScale.x), transform.position.y + redYoffset), new Vector2(redXSize, redYSize), 0f, ledge);
-         //if the variable is public static and exists on the same object, you can access it with the name of the script!!
+            RedBox = Physics2D.OverlapBox(new Vector2(transform.position.x + (redXOffset * transform.localScale.x), transform.position.y + redYoffset), new Vector2(redXSize, redYSize), 0f, ledge);
+            //if the variable is public static and exists on the same object, you can access it with the name of the script!!
+        }
+        else
+        {
+            greenXOffset = 0.09f;
+            redXOffset = .1f;
+            greenBox = Physics2D.OverlapBox(new Vector2(transform.position.x + (greenXOffset * transform.localScale.x), transform.position.y + greenYOffset), new Vector2(greenXsize, greenYSize), 0f, ledge);
+            //we dont need GreenYOffset* transform.localscale.y because the Y axis is fixed when rotating on X.axis, but we do need it for the X axis
 
-        if(greenBox && !RedBox && !Movement.isGrabbing && !isOntheGround())
+            RedBox = Physics2D.OverlapBox(new Vector2(transform.position.x + (redXOffset * transform.localScale.x), transform.position.y + redYoffset), new Vector2(redXSize, redYSize), 0f, ledge);
+            //if the variable is public static and exists on the same object, you can access it with the name of the script!!
+
+        }
+
+        if (greenBox && !RedBox && !Movement.isGrabbing && !isOntheGround())
         {
             Movement.isGrabbing = true;
         }
@@ -39,7 +54,7 @@ public class LedgeGrab : MonoBehaviour
         if(Movement.isGrabbing)
         {
             anim.SetBool("LedgeGrab", true);
-            rb.velocity = new Vector2(0f, 0f); //setting the x and y velocity to zero  (even i was doing the same in my implementation!)
+            rb.velocity = new Vector2(0, 0);//setting the x and y velocity to zero  (even i was doing the same in my implementation!)
             rb.gravityScale = 0f;  //and sets the gravity scale to zero
         }
         if(anim.GetCurrentAnimatorStateInfo(0).IsName("LedgeGrab") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime>=.5f) //it checks for the same animations normalziaed TIME!!
@@ -52,8 +67,17 @@ public class LedgeGrab : MonoBehaviour
 
     public void ChangePositionOfThePlayer()
     {
-        transform.position = new Vector2(transform.position.x + 5 * transform.localScale.x *Time.deltaTime, transform.position.y + 10 * Time.deltaTime) ;
-        Debug.Log("Ledge!");
+        if(sr.flipX)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y +12 * Time.deltaTime * transform.localScale.y);
+            transform.position = new Vector2(transform.position.x - 10 * Time.deltaTime * transform.localScale.x, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + 12 * Time.deltaTime * transform.localScale.y);
+            transform.position = new Vector2(transform.position.x + 10 * Time.deltaTime * transform.localScale.x, transform.position.y);
+        }
+       
         rb.gravityScale = startingGrav;
         Movement.isGrabbing = false;
 
