9344f2afcc8173c29915a8d40f033934f3931da4
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2022-12-12 13:26:32 +0300

    ss

---
+++ Assets/EnemyJumping.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyJumping : MonoBehaviour
+{
+
+    //ENEMY JUMPING FROM ONE PLATFORM TO ANOTHER
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/LedgeGrab.cs
+++ Assets/LedgeGrab.cs
@@ -38,7 +38,7 @@ public class LedgeGrab : MonoBehaviour
 
         }
 
-        greenBox = Physics2D.OverlapBox(new Vector2(transform.position.x + (redXOffset * transform.localScale.x), transform.position.y), new Vector2(redXSize, redYSize), 0, ledge);
+        greenBox = Physics2D.OverlapBox(new Vector2(transform.position.x + (greenXOffset * transform.localScale.x), transform.position.y + greenYOffset), new Vector2(greenXsize, greenYSize), 0, ledge);
         //we dont need GreenYOffset* transform.localscale.y because the Y axis is fixed when rotating on X.axis, but we do need it for the X axis
 
         RedBox = Physics2D.OverlapBox(new Vector2(transform.position.x + (redXOffset * transform.localScale.x), transform.position.y + redYoffset),
