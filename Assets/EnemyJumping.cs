3d14fbded63583c9e476acd572fbbfea49cdc8ca
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2022-12-15 14:57:33 +0300

    ss

--- Assets/EnemyJumping.cs
+++ Assets/EnemyJumping.cs
@@ -8,18 +8,23 @@ public class EnemyJumping : MonoBehaviour
     //ENEMY JUMPING FROM ONE PLATFORM TO ANOTHER
     private Rigidbody2D rb;
     private Animator anim;
-    public ArrayList TraversalPoints = new ArrayList();
+    private RaycastHit2D hit;
+    [SerializeField] LayerMask Jumping;
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
         anim=GetComponent<Animator>();
-        TraversalPoints.Add(GameObject.FindWithTag("Jump"));
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+        hit = Physics2D.Raycast(transform.position, transform.right, 1.5f, Jumping);
+        Debug.DrawRay(transform.position, transform.right * 1.5f, Color.red);
+        if(hit.collider!=null && hit.collider.isTrigger)
+        {
+            Debug.Log("JumpYES");
+        }
     }
 
     private void FixedUpdate()
