c9dd28ededfa89952c9b5af7b41b42a230cd9e9d
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2022-12-29 12:21:59 +0300

    ss

--- Assets/AttackPlayer.cs
+++ Assets/AttackPlayer.cs
@@ -7,6 +7,7 @@ public class AttackPlayer : MonoBehaviour
    
     [SerializeField] LayerMask player;
     private Animator anim;
+    private int multiplier = -1;
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -15,16 +16,34 @@ public class AttackPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(transform.position, transform.right * 1f, Color.black);
 
-        if ( Physics2D.Raycast(transform.position, transform.right, 1f, player ))
+        if (GetComponent<SpriteRenderer>().flipX == false)
+        {
+            multiplier=1;
+        }
+        else
+        {
+            multiplier =-1;
+
+        }
+
+        Debug.DrawRay(transform.position, (multiplier)*transform.right * 1f, Color.black);
+
+        if (Physics2D.Raycast(transform.position, (multiplier) * transform.right, 1f, player))
         {
             EnemyJumping.Attacking = true;
             anim.SetBool("Attack", true);
             anim.SetBool("CanWalk", false);
 
         }
+        else
+        {
+            EnemyJumping.Attacking = false;
 
-       
+            anim.SetBool("Attack", false);
+            anim.SetBool("CanWalk", true);
+        }
+
+
     }
 }
