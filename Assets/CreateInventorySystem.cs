c7f75853592f02580df75a92aa70942f5ec877be
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-04-12 11:37:24 +0300

    Inventory

--- Assets/CreateInventorySystem.cs
+++ Assets/CreateInventorySystem.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CreateInventorySystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject PanelObject;
+    [SerializeField] GameObject InventoryBox;
+
+    [Space]
+    [Header("Make sure the size doesnt exceed the screen")]
+    [SerializeField] int SizeOftheInventory;
+
+    private RectTransform _spriteLocation;
+
+    
+
     void Start()
     {
-        
+        _spriteLocation=PanelObject.GetComponent<RectTransform>();
+
+        StartCoroutine(GenerateInventory(SizeOftheInventory));
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    IEnumerator GenerateInventory(int Size)
     {
-        
+        int increment = -250;
+        int decrement = 150;
+
+        for (int i=0; i<Size; i++)
+        {
+            for(int j=0; j<Size; j++)
+            {
+                Debug.Log(_spriteLocation.position);
+                Vector3 IncrementalSize = new Vector3(increment, decrement);
+                GameObject _temp= Instantiate(InventoryBox, IncrementalSize, Quaternion.identity);
+                _temp.transform.SetParent(PanelObject.transform,false);
+                increment += 100;
+
+            }
+            decrement -= 50;
+            increment = -250;
+        }
+
+
+        yield return null;
     }
+
 }
