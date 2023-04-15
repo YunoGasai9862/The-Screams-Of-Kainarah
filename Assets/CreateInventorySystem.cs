80e38a4f8d50a02887a1d6e2a10b5c9a74acbd1b
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-04-15 10:42:51 +0300

    FixedDuplicateInventoryItems

--- Assets/CreateInventorySystem.cs
+++ Assets/CreateInventorySystem.cs
@@ -1,4 +1,3 @@
-using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -11,8 +10,10 @@ public class CreateInventorySystem : MonoBehaviour
     [SerializeField] GameObject InventoryBox;
     [SerializeField] Canvas canvas;
     private static Queue<GameObject> inventoryList;
-   
-
+    private static Queue<GameObject> inventoryCheck;
+    private static Queue<GameObject> inventoryTemp;
+    private static int i = 0;
+    private static bool _alreadyExist=false;
     private int SizeOftheInventory=6;
 
     [Space]
@@ -32,6 +33,8 @@ public class CreateInventorySystem : MonoBehaviour
     void Start()
     {
         inventoryList=new Queue<GameObject>();
+        inventoryCheck=new Queue<GameObject>();
+        inventoryTemp=new Queue<GameObject>();
         _spriteLocation=PanelObject.GetComponent<RectTransform>();
 
         if(startX==0 && startY==0 && increment==0 && decrement==0)
@@ -69,24 +72,73 @@ public class CreateInventorySystem : MonoBehaviour
 
     public static bool AddToInventory(Sprite itemTobeAdded)
     {
-        GameObject _temp=new GameObject();
+        GameObject _temp = new GameObject("Item" + i);
+        _temp.transform.localScale = new Vector3(.35f, .35f, .35f);
 
-        if (inventoryList.Count==0)
-           {
-               return false;
-           }
+        if (inventoryList.Count == 0)
+        {
+            return false;
+        }
+
 
         GameObject ItemBox = inventoryList.Dequeue();
-        RectTransform RT= _temp.AddComponent<RectTransform>();
-        _temp.transform.SetParent(ItemBox.transform);
-        UnityEngine.UI.Image image = _temp.AddComponent<UnityEngine.UI.Image>();
-        image.sprite = itemTobeAdded;
-        _temp.GetComponent<SpriteRenderer>().sprite=itemTobeAdded;
+        if(inventoryCheck.Count!=0)
+        {
+            while (inventoryCheck.Count != 0 && !_alreadyExist)
+            {
+                GameObject ExistingInventory = inventoryCheck.Dequeue();
+                if (ExistingInventory.GetComponent<UnityEngine.UI.Image>().sprite == itemTobeAdded)
+                {
+                    _alreadyExist = true;
+                    inventoryTemp.Enqueue(ExistingInventory);
+                    break;
+                }
+                else
+                {
+                    _alreadyExist = false;
 
+                }
+
+
+            }
+            TransferTheItemsToQueue(inventoryCheck, inventoryTemp);
+
+        }
+        
+        if(!_alreadyExist)
+        {
+
+            RectTransform RT = _temp.AddComponent<RectTransform>();
+            _temp.transform.SetParent(ItemBox.transform, false);
+            UnityEngine.UI.Image image = _temp.AddComponent<UnityEngine.UI.Image>();
+            image.sprite = itemTobeAdded;
+            i++;
+            inventoryCheck.Enqueue(_temp);
+
+        }
 
         return true;
 
 
+    }
+
+    public static void TransferTheItemsToQueue(Queue<GameObject> queue1, Queue<GameObject> queue2)
+    {
+        if(queue1.Count==0)
+        {
+            Exchange(queue2, queue1);
+        }else
+        {
+            Exchange(queue1, queue2);
+        }
+    }
+
+    public static void Exchange(Queue<GameObject> queue1, Queue<GameObject> queue2)
+    {
+        while(queue1.Count != 0)
+        {
+            queue2.Enqueue(queue1.Dequeue());
+        }
     }
 
 }
