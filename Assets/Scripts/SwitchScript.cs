fb1eac2dc438a36af02b2dfbd9ec4c511bfc2ca7
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-05-16 15:33:16 +0300

    Toggle

--- Assets/Scripts/SwitchScript.cs
+++ Assets/Scripts/SwitchScript.cs
@@ -2,25 +2,43 @@
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
+using Unity.VisualScripting;
+
 public class SwitchScript : MonoBehaviour
 {
     private Toggle _toggle;
     [SerializeField] RectTransform _uiHandler;
-    [SerializeField] Color _color;
-    private Vector2 AnchorPoint;
-    void Start()
-    {
-        _toggle= GetComponent<Toggle>();   
-        
-    }
+    [SerializeField] Image BG;
+    [SerializeField] Image HandlerColor;
+    [SerializeField] Color HandleColor;
+    [SerializeField] Color _background;
+    [SerializeField] Color _transitionBackground;
+    [SerializeField] Color _transitionHandler;
 
-    // Update is called once per frame
-    void Update()
+    private Vector2 AnchorPoint;
+ 
+    private void Awake()
     {
+        _toggle = GetComponent<Toggle>();
+
+        AnchorPoint = _uiHandler.anchoredPosition;
+        BG.color = _background;
+        HandlerColor.color = HandleColor;
+        _toggle.onValueChanged.AddListener(ChangeToggle); //delegates/adds listener
+
         if(_toggle.isOn)
         {
-            AnchorPoint = _uiHandler.anchoredPosition;
-            _uiHandler.DOAnchorPos(AnchorPoint * -1, 1f);
+            ChangeToggle(true);  //if its true, passes true
+            //when false, it automatically delegates the false one
         }
+    }
+    // Update is called once per frame
+  
+    public void ChangeToggle(bool isOn)
+    {
+        _uiHandler.DOAnchorPos(isOn ? AnchorPoint * -1 : AnchorPoint,.4f).SetEase(Ease.InFlash);
+        BG.DOColor(isOn ? _transitionBackground : _background, .4f).SetEase(Ease.InFlash);
+        HandlerColor.DOColor(isOn ? _transitionHandler : HandleColor, .4f).SetEase(Ease.InFlash);
+
     }
 }
