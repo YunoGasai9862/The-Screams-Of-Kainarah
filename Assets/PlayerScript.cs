610e054cd2657e279c630023893b8d3ba0bb0623
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-05-28 12:13:10 +0300

    InputSystem

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerScript : MonoBehaviour
+{
+
+    public void Jump()
+    {
+        Debug.Log("Jump");
+    }
+}
