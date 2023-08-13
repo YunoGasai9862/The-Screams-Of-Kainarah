9ec39c488bda8a09038646dccbaca4032d2ddee8
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-08-14 00:33:28 +0300

    some modifications

--- Assets/RunAsyncCoroutine.cs
+++ Assets/RunAsyncCoroutine.cs
@@ -1,22 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class RunAsyncCoroutine : MonoBehaviour
+public class RunAsyncCoroutine : MonoBehaviour //attach it to the a GameObject
 {
+    private static RunAsyncCoroutine instance;
+
+    private readonly Queue<Func<IAsyncEnumerator<WaitForSeconds>>> asyncEnumeratorCollection = new();
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public static void RunTheAsyncCoroutine(Func<IAsyncEnumerator<WaitForSeconds>> asyncEnumerator)
+    {
+        instance.asyncEnumeratorCollection.Enqueue(asyncEnumerator); //adds it to the Queue
+    }
+    public static RunAsyncCoroutine RunAsyncCoroutineInstance { get => instance; set => instance = value; } //getter + setter
     public static void StartAsyncCoroutine(IAsyncEnumerator<WaitForSeconds> asyncCoroutine)
     {
-        async IEnumerator runningAsyncCouroutine(IAsyncEnumerator<WaitForSeconds> enumerator)
-        {
-            while (await enumerator.MoveNextAsync())
-            {
-                yield return enumerator.Current;
-            }
 
-        }
 
     }
+
+    public static void AttachToGameObject()
+    {
+        var coroutineRunner = new GameObject("AsyncCoroutineRunner").AddComponent<RunAsyncCoroutine>();
+        //run the script!
+    }
+
+
 
 
 }
