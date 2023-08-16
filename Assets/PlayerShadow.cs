fd96ac4c784fdab4554f30460dcb901022511c15
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-08-16 16:17:27 +0300

    fix_tomorrow

--- Assets/PlayerShadow.cs
+++ Assets/PlayerShadow.cs
@@ -10,35 +10,37 @@ public class PlayerShadow : MonoBehaviour
     private SpriteRenderer m_SpriteRenderer;
     private Rigidbody2D m_Rigidbody;
     private bool isFixed = false;
-    private Vector2 m_previousResults;
+    Vector2 newPosition;
+    private Vector2 m_PreviousPos;
     private void Awake()
     {
         m_Position = transform.position;
         m_SpriteRenderer = transform.parent.GetComponent<SpriteRenderer>();
-        m_previousResults = transform.position;
+        m_PreviousPos = transform.position;
     }
     // Update is called once per frame
     async void Update()
     {
-        Vector2 newPosition = await ShadowObjectsNewPosition(m_SpriteRenderer, m_Position, .5f);
+        newPosition = await ShadowObjectsNewPosition(m_SpriteRenderer, m_Position, .5f);
 
         if (!isFixed)
         {
-            m_previousResults = newPosition;
             transform.position = new Vector2(newPosition.x, newPosition.y); //updates it
+            m_Position = transform.position;
         }
+
     }
 
     private async Task<Vector2> ShadowObjectsNewPosition(SpriteRenderer spriteRenderer, Vector2 position, float offset)
     {
         Vector2 result = HelperFunctions.FlipTheObjectToFaceParent(ref spriteRenderer, position, offset);
 
-        if (result.magnitude == m_previousResults.magnitude)
+        if (m_PreviousPos.magnitude == result.magnitude)
             isFixed = true;
         else
             isFixed = false;
 
-        Debug.Log(result + " " + m_previousResults);
+        m_PreviousPos = result;
 
         await Task.Delay(200); //to not let it calculate everytime
 
