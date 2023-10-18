43c44ce66494f2cdafc808a9c7d23164b9b1acbb
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-10-18 12:03:09 +0300

    Some updates

--- Assets/EnemyWalkBetweenPathsAI.cs
+++ Assets/EnemyWalkBetweenPathsAI.cs
@@ -16,11 +16,17 @@ public class EnemyWalkBetweenPathsAI : MonoBehaviour
     [Header("Custom Behavior")]
     public bool isFollowEnabled;
     public bool isJumpEnabled;
+    public LayerMask layerMaskForGrounding;
+    public float forceMagnitude;
 
     private Seeker seeker;
     private Rigidbody2D rb;
     private Path path;
-    private int currentIndex=0;
+    private int currentIndex=-1;
+    private int sign;
+    private int currentWayPointIndex = 0;
+    private Transform selectedTargetToMoveToward;
+    private bool isGrounded;
     void Start()
     {
         seeker = GetComponent<Seeker>();
@@ -41,31 +47,54 @@ public class EnemyWalkBetweenPathsAI : MonoBehaviour
     { 
        if(await IsInVisibleDistance() && isFollowEnabled && seeker.IsDone()) //if one path is finished
         {
-          //  seeker.StartPath(rb.position, )
+            seeker.StartPath(rb.position, WayPoints[currentIndex].position, OnPathComplete);
         }
      
     }
 
     private async Task<bool> IsInVisibleDistance() //find the closest first
     {
-        int sign;
-        if(currentIndex < WayPoints.Length)
+        if(currentIndex < WayPoints.Length-1 || currentIndex < 0)
         {
             sign = 1;
         }else
         {
             sign = -1;
         }
-
-        bool isInDistance = Vector2.Distance(transform.position, WayPoints[currentIndex].position) < activeDistance;
         currentIndex = currentIndex + sign;
+        bool isInDistance = Vector2.Distance(transform.position, WayPoints[currentIndex].position) < activeDistance;
+        if(isInDistance)
+         selectedTargetToMoveToward= WayPoints[currentIndex].transform; 
         await Task.Delay(500);
         return isInDistance;
 
     }
 
+    private void OnPathComplete(Path p) 
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWayPointIndex = 0;//resets (this index is for all the waypoints between the wayPoints i have specified)
+        }
+    }
+
     private async void PathToFollow()
     {
+        if(path==null)
+        {
+            return; //there's an error -> exit (nothing to follow)
+        }
+
+        if(currentWayPointIndex > path.vectorPath.Count)
+        {
+            return; //crossed all waypoints so far
+        }
+
+        isGrounded = Physics2D.Raycast(rb.position, -Vector3.up, 1f, layerMaskForGrounding);
+
+        Vector3 direction = ((Vector2)path.vectorPath[currentWayPointIndex]- rb.position).normalized;  //the waypoint index in the path selected for that true value
+        Vector3 force = direction * rb.mass * forceMagnitude;
 
     }
 }
