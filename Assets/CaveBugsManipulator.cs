45d42f5861f4522bb7513a53b02842dfb5aa52a6
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-10-25 12:12:45 +0300

    Particle Updates

---
+++ Assets/CaveBugsManipulator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveBugsManipulator : MonoBehaviour
+{
+    [Header("Particle System")]
+    [SerializeField] ParticleSystem particleSystem;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
