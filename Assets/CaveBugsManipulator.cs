e68b8207c458c339346230f2ee525596bcb517a1
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-10-26 12:00:28 +0300

    Bloom upates + particle script started

--- Assets/CaveBugsManipulator.cs
+++ Assets/CaveBugsManipulator.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class CaveBugsManipulator : MonoBehaviour
 {
     [Header("Particle System")]
-    [SerializeField] ParticleSystem particleSystem;
-    void Start()
+    [SerializeField] ParticleSystem _ps;
+
+    private void Start()
     {
-        
+        _ps.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    [System.Obsolete]
+    async void Update()
     {
+        await gravityModifier();
+    }
+
+    [System.Obsolete]
+    private async Task<bool> gravityModifier()
+    {
+        _ps.gravityModifier -= .1f;
+        await Task.Delay(1000);
+        return true;
         
     }
 }
