405e9506af917b2811e4810311814eacccd530e1
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-10-27 12:23:07 +0300

    Particle Update started + some assets

--- Assets/CaveBugsManipulator.cs
+++ Assets/CaveBugsManipulator.cs
@@ -1,30 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class CaveBugsManipulator : MonoBehaviour
 {
     [Header("Particle System")]
     [SerializeField] ParticleSystem _ps;
+    [SerializeField] Transform _target;
+    private MainModule mainModule;
+    private Particle[] particles;
+    private CancellationTokenSource cancellationTokenSource;
+    private CancellationToken cancellationToken;
+
+    private void Awake()
+    {
+        cancellationTokenSource = new CancellationTokenSource();
+        cancellationToken = cancellationTokenSource.Token;
+    }
+
 
     private void Start()
     {
+        mainModule = _ps.main;
         _ps.Play();
     }
 
-    [System.Obsolete]
     async void Update()
     {
-        await gravityModifier();
+        //await gravityModifier();
+        particles = new Particle[mainModule.maxParticles]; //stores particles (updates their position as well)
+
+       int numberOfParticlesUpdated = _ps.GetParticles(particles); //updates the state of the particles.
+
+        await travelTowardTarget(particles);
     }
 
-    [System.Obsolete]
-    private async Task<bool> gravityModifier()
+    private async Task gravityModifier()
     {
-        _ps.gravityModifier -= .1f;
         await Task.Delay(1000);
-        return true;
-        
+        mainModule.gravityModifierMultiplier -= .001f;
     }
+
+    private async Task travelTowardTarget(Particle[] particles)
+    {
+        for(int i= 0; i < particles.Length; i++)
+        {
+            await Task.Delay(500);
+            if(!cancellationToken.IsCancellationRequested)
+             Debug.Log(particles[i].position);
+        }
+    }
+
+    private void OnDisable()
+    {
+        cancellationTokenSource.Cancel();
+    }
+
 }
