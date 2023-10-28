e39893c3f27bf5ce67db17a6db00aaf3a7c9fbcb
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-10-28 15:33:40 +0300

    Fix it tomorrow

--- Assets/CaveBugsManipulator.cs
+++ Assets/CaveBugsManipulator.cs
@@ -14,6 +14,7 @@ public class CaveBugsManipulator : MonoBehaviour
     private Particle[] particles;
     private CancellationTokenSource cancellationTokenSource;
     private CancellationToken cancellationToken;
+    private bool _taskRunning = false;
 
     private void Awake()
     {
@@ -25,17 +26,37 @@ public class CaveBugsManipulator : MonoBehaviour
     private void Start()
     {
         mainModule = _ps.main;
+        particles = new Particle[mainModule.maxParticles]; //stores particles (updates their position as well)
         _ps.Play();
+
     }
 
     async void Update()
     {
         //await gravityModifier();
-        particles = new Particle[mainModule.maxParticles]; //stores particles (updates their position as well)
 
-       int numberOfParticlesUpdated = _ps.GetParticles(particles); //updates the state of the particles.
+        int numberOfParticlesUpdated = _ps.GetParticles(particles); //updates the state of the particles.
 
-        await travelTowardTarget(particles);
+        if (numberOfParticlesUpdated > 0 && !_taskRunning) //we dont have the particles
+        {
+          // _taskRunning = true;
+          //_taskRunning = await travelTowardTarget(particles);
+
+        }
+
+        for (int i = 0; i < particles.Length; i++)
+        {
+            await Task.Delay(10);
+            if (!cancellationToken.IsCancellationRequested)
+            {
+
+              particles[i].position = Vector2.MoveTowards(particles[i].position, _target.position, Time.deltaTime);
+             _ps.SetParticles(particles);
+
+            }
+        }
+
+
     }
 
     private async Task gravityModifier()
@@ -44,14 +65,17 @@ public class CaveBugsManipulator : MonoBehaviour
         mainModule.gravityModifierMultiplier -= .001f;
     }
 
-    private async Task travelTowardTarget(Particle[] particles)
+    private async Task<bool> travelTowardTarget(Particle[] particles)
     {
         for(int i= 0; i < particles.Length; i++)
         {
-            await Task.Delay(500);
-            if(!cancellationToken.IsCancellationRequested)
-             Debug.Log(particles[i].position);
+            await Task.Delay(10);
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                particles[i].position = Vector2.MoveTowards(particles[i].position, _target.position, Time.deltaTime);
+            }
         }
+        return false;
     }
 
     private void OnDisable()
