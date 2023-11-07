040e7b1b0a233251dfd7eec41d695229027e0434
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-11-07 12:23:54 +0300

    More light updates + HDR enabled + semophore

--- Assets/MoonFlickering.cs
+++ Assets/MoonFlickering.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -9,16 +7,19 @@ public class MoonFlickering : LightObserverPattern
 {
     [SerializeField]
     public bool canFlicker;
+    public float innerRadiusMin;
+    public float innerRadiusMax;
+    public float outerRadiusMin;
+    public float outerRadiusMax;
 
     private LightEntity m_light= new LightEntity();
     private CancellationTokenSource m_cancellationTokenSource;
     private CancellationToken m_cancellationToken;
     private SemaphoreSlim _semaphoreSlim;
-    private bool canProceed=true;
     private void Awake()
     {
         m_cancellationTokenSource = new CancellationTokenSource();
-        _semaphoreSlim= new SemaphoreSlim(0);
+        _semaphoreSlim= new SemaphoreSlim(1); //i already have one semaphoreSlim with 0 in another script, hence initializing it with 1
         m_cancellationToken = m_cancellationTokenSource.Token;
         m_light.canFlicker = canFlicker;
         m_light.LightName = transform.parent.name; //always add transform.parent.name
@@ -26,25 +27,35 @@ public class MoonFlickering : LightObserverPattern
     }
     async void Update()
     {
-        if(canProceed)
-         await MoonShimmer(m_light, m_cancellationToken);
+         await MoonShimmer(m_light, m_cancellationToken, innerRadiusMin, innerRadiusMax, outerRadiusMin, outerRadiusMax);
     }
 
-    private async Task MoonShimmer(LightEntity entity, CancellationToken cancellationToken)
+    private async Task<float> GenerateRadia(float min, float max)
     {
-        canProceed = false;
+        await Task.Delay(TimeSpan.FromSeconds(0.5));
+        return UnityEngine.Random.Range(min, max);
+    }
+
+    private async Task MoonShimmer(LightEntity entity, CancellationToken cancellationToken, float innerMinRadius, float innerMaxRadius, float outerMinRadius, float outerMaxRadius)
+    {
+        await _semaphoreSlim.WaitAsync(); //waits for the thread to become available
         if (!m_cancellationTokenSource.IsCancellationRequested)
         {
-            await Task.Delay(TimeSpan.FromSeconds(3));
+            await Task.Delay(TimeSpan.FromSeconds(.5));
 
             try
             {
-                await NotifyAllLightObserversAsync(entity, cancellationToken, _semaphoreSlim);
-                canProceed=true;
+                await GenerateRadia(innerMinRadius, innerMaxRadius);
+                await GenerateRadia(outerMinRadius, outerMaxRadius);
+                await NotifyAllLightObserversAsync(entity, cancellationToken);
             }
             catch (OperationCanceledException) //catches the exception, and gracefully exits
             {
                 return;
+            }
+            finally {
+
+                _semaphoreSlim.Release(); //it releases, allowing it to run again
             }
         }
     }
