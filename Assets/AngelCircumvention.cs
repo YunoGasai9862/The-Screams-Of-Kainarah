afb76fa2f7f1891b3866b2a27896eeae6a4ff53f
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-11-16 12:06:11 +0300

    Sprite Sheet + Rotation

--- Assets/AngelCircumvention.cs
+++ Assets/AngelCircumvention.cs
@@ -3,38 +3,59 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AngelCircumvention : MonoBehaviour
 {
     [Header("Custom Variables")]
-    [SerializeField] float centerPoint;
-    [SerializeField] float radius;
+    [SerializeField] Vector2 centerPoint;
+    [SerializeField] double radius;
 
     private SemaphoreSlim semaphore;
     private CancellationTokenSource cancellationTokenSource;
     private CancellationToken cancellationToken;
 
-    private float angle = 0;
-    void Start()
+    private double angle = 0;
+
+    private void Start()
     {
-        
+        semaphore = new SemaphoreSlim(1);
+        cancellationTokenSource = new CancellationTokenSource();
+        cancellationToken = cancellationTokenSource.Token;
     }
 
     // Update is called once per frame
-    void Update()
+    async void Update()
     {
-        
+        await semaphore.WaitAsync();
+
+        if(!cancellationToken.IsCancellationRequested)
+        {
+            angle += Time.deltaTime;
+            transform.position = await CircumventAroundAPoint(angle, centerPoint, radius, semaphore);
+        }
     }
 
-    private async Task CircumventAroundAPoint(float center, float radius)
+    private async Task<Vector2> CircumventAroundAPoint(double angle, Vector2 center, double radius, SemaphoreSlim semaphore)
     {
-
+        //rcosQ and rsinQ
+        double angleTemp = await checkAngle(angle);
+        Vector2 position = new Vector2((float)(center.x + radius * Math.Cos(angleTemp)), (float)(center.y + radius * Math.Sin(angleTemp)));
+        await Task.Delay(TimeSpan.FromSeconds(0));
+        semaphore.Release();
+        return position;
     }
 
-    private async Task<float> checkAngle(float angle)
+    private async Task<double> checkAngle(double angle)
     {
         await Task.Delay(TimeSpan.FromSeconds(0));
         return angle > 360 ? 0 : angle;
+    }
+
+    private void OnDisable()
+    {
+        transform.position = centerPoint;
+        cancellationTokenSource.Cancel();
     }
 }
