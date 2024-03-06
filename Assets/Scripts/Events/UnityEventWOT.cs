07d2e0f0827fc54239e4704c2979fdc4b1cf1e99
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2024-03-06 11:23:44 +0300

    Continue working on generic/non-generic events for getting instance + add them to ledge grab + throw dagger + enhance ledge grab + AI images + Shaders!!!

--- Assets/Scripts/Events/UnityEventWOT.cs
+++ Assets/Scripts/Events/UnityEventWOT.cs
@@ -1,16 +1,7 @@
 using UnityEngine.Events;
 
 //without type <T>
-public class UnityEventWOT : UnityEvent //extends the base class, but adds GetInstance functionality
+public abstract class UnityEventWOT : UnityEvent //extends the base class, but adds GetInstance functionality
 {
-    private static UnityEventWOT _event;
-    public UnityEventWOT() { }
-    public static UnityEventWOT GetInstance()
-    {
-        if (_event == null)
-        {
-            _event = new UnityEventWOT();
-        }
-        return _event;
-    }
+    public abstract UnityEventWOT GetInstance();
 }
--- Assets/Scripts/Events/UnityEventWT.cs
+++ Assets/Scripts/Events/UnityEventWT.cs
@@ -1,18 +1,7 @@
 using UnityEngine.Events;
 
 //with type <T>
-public class UnityEventWT<T> : UnityEvent<T> //extends the base class, but adds GetInstance functionality
+public abstract class UnityEventWT<T> : UnityEvent<T> //extends the base class, but adds GetInstance functionality
 {
-    private static UnityEventWT<T> _event;
-
-    private UnityEventWT() { }
-
-    public static UnityEventWT<T> GetInstance()
-    {
-        if(_event == null)
-        {
-            _event = new UnityEventWT<T>();
-        }
-        return _event;
-    }
+    public abstract UnityEventWT<T> GetInstance();
 }
