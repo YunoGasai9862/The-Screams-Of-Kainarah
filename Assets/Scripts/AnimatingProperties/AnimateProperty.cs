bd758867779835aa9114b413cdcb4f51b72fc4d0
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2024-04-09 16:32:53 +0300

    Tweak player gravity + animation class continue (library) + Health animatiosn + game improvement!!!!

--- Assets/Scripts/AnimatingProperties/AnimateProperty.cs
+++ Assets/Scripts/AnimatingProperties/AnimateProperty.cs
@@ -1,13 +1,54 @@
+using System;
+using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 public class AnimateProperty : IAnimateProperty
 {
-    public Color AnimColor(Color color, float duration)
+
+    IEnumerator IAnimateProperty.AnimColor(Color initialColor, Color endColor, float duration)
     {
-        throw new System.NotImplementedException();
+        Color difference = GetDifference<Color>(initialColor, endColor);
+        Debug.Log(difference);
+        yield return new WaitForSeconds(0.5f);
     }
 
-    public Transform AnimMovement(Transform transform, float duration)
+    IEnumerator IAnimateProperty.AnimMovement(Vector3 initialTransform, Vector3 endTransform, float duration)
     {
-        throw new System.NotImplementedException();
+        Vector3 difference = GetDifference<Vector3>(initialTransform, endTransform);
+        yield return new WaitForSeconds(0.5f);
     }
+    public T GetDifference<T>(T initialValue, T finalValue)
+    {
+        switch (typeof(T))
+        {
+            case Type t when t == typeof(Color):
+                Color begValueC = (Color)Convert.ChangeType(initialValue, typeof(Color));
+                Color endValueC = (Color)Convert.ChangeType(finalValue, typeof(Color));
+                Color differenceC = new Color(endValueC.r - begValueC.r, endValueC.g - begValueC.g, endValueC.b - begValueC.b, endValueC.a - begValueC.a);
+                return (T) Convert.ChangeType(differenceC, typeof(T));
+
+            case Type t when t == typeof(Vector3):
+                Vector3 begValueV = (Vector3)Convert.ChangeType(initialValue, typeof(Vector3));
+                Vector3 endValueV = (Vector3)Convert.ChangeType(finalValue, typeof(Vector3));
+                Vector3 differenceV = new Vector3(endValueV.x - begValueV.x, endValueV.y - begValueV.y, endValueV.z - begValueV.z);
+                return (T)Convert.ChangeType(differenceV, typeof(T));
+
+
+            case Type t when t == typeof(Vector2):
+                Vector2 begValueV2 = (Vector2)Convert.ChangeType(initialValue, typeof(Vector3));
+                Vector2 endValueV2 = (Vector3)Convert.ChangeType(finalValue, typeof(Vector3));
+                Vector2 differenceV2 = new Vector2(endValueV2.x - begValueV2.x, endValueV2.y - begValueV2.y);
+                return (T)Convert.ChangeType(differenceV2, typeof(T));
+
+            default:
+                break;
+        }
+        return (T)Convert.ChangeType(null, typeof(T)); 
+    }
+
+    public void SetTarget<T>()
+    {
+
+    }
+
 }
--- Assets/Scripts/AnimatingProperties/IAnimateProperty.cs
+++ Assets/Scripts/AnimatingProperties/IAnimateProperty.cs
@@ -1,7 +1,10 @@
 ﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 public interface IAnimateProperty
 {
-    public abstract Color AnimColor(Color color, float duration);
-    public abstract Transform AnimMovement(Transform transform, float duration);
+    public abstract IEnumerator AnimColor(Color initialColor, Color endColor, float duration);
+    public abstract IEnumerator AnimMovement(Vector3 initialTransform, Vector3 endTransform, float duration);
 }
