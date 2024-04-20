843e84ba8aa910cf78c4de9144f8278602927969
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2024-04-20 16:32:39 +0300

    Keep working on chunk sizes + potentially lerp + Rakash controller + more animations + improvement!!!

--- Assets/Scripts/AnimatingProperties/AnimatePropertyColor.cs
+++ Assets/Scripts/AnimatingProperties/AnimatePropertyColor.cs
@@ -17,19 +17,23 @@ public class AnimatePropertyColor : IAnimatePropertyColor<Color>
 
     public IEnumerator AnimColor(Color initialColor, Color endColor, float duration)
     {
-        
+
         Color difference = GetDifference(initialColor, endColor);
-        Color newValue = new Color(0,0,0,0);
-        Color sign;
-        while (TimePassed < duration && CheckIfUpdatedColorValueIsWithinRange(newValue, endColor, difference))
+        Color newValue = new Color(0, 0, 0, 0);
+        Color sign = CalculateSign(difference);
+        Debug.Log(sign.a);
+        Color chunkSize = CalculateChunkSize(duration, difference);
+        Color tempChunkSize = chunkSize;
+        while (CheckIfUpdatedColorValueIsWithinRange(newValue, endColor, difference))
         {
             TimePassed += Time.deltaTime;
-            //utilize chunk size now
-            Color chunkSize = CalculateChunkSize(duration, difference);
-            sign = CalculateSign(difference);
-            newValue = new Color(initialColor.r + (sign.r * chunkSize.r), initialColor.g + (sign.g * chunkSize.g), initialColor.b + (sign.b * chunkSize.b), initialColor.a + (sign.a * chunkSize.a));
+            float progress = Mathf.Clamp(TimePassed, 0, duration);
+            Debug.Log($"Progress: {progress}");
+            newValue = new Color(initialColor.r + (sign.r * tempChunkSize.r * TimePassed), initialColor.g + (sign.g * tempChunkSize.g * TimePassed), initialColor.b + (sign.b * tempChunkSize.b * TimePassed), initialColor.a + (sign.a * tempChunkSize.a * TimePassed));
+            tempChunkSize = tempChunkSize + chunkSize;
+            Debug.Log($"New Chunk Size = {chunkSize} TimePassed = {TimePassed}");
             _notifyColorChangeEvent.Invoke(newValue);
-            yield return new WaitForSeconds(0f);
+            yield return null;
         }
 
         TimePassed = 0;
@@ -39,12 +43,12 @@ public class AnimatePropertyColor : IAnimatePropertyColor<Color>
     {
         return new Color(finalValue.r - initialValue.r, finalValue.g - initialValue.g, finalValue.b - initialValue.b, finalValue.a - initialValue.a);
     }
-    
+
     public Color CalculateSign(Color differenceValue)
     {
-        Color sign = new Color(0,0,0,0);
+        Color sign = new Color(0, 0, 0, 0);
         sign.r = differenceValue.r == 0 ? 0 : differenceValue.r > 0 ? 1 : -1;
-        sign.g = differenceValue.g == 0 ? 0 : differenceValue.g > 0 ? 1: -1;
+        sign.g = differenceValue.g == 0 ? 0 : differenceValue.g > 0 ? 1 : -1;
         sign.b = differenceValue.b == 0 ? 0 : differenceValue.b > 0 ? 1 : -1;
         sign.a = differenceValue.a == 0 ? 0 : differenceValue.a > 0 ? 1 : -1;
         return sign;
@@ -62,10 +66,10 @@ public class AnimatePropertyColor : IAnimatePropertyColor<Color>
     public Color CalculateChunkSize(double durationInSeconds, Color difference)
     {
         TimeSpan duration = TimeSpan.FromMilliseconds(durationInSeconds * 1000f);
-        return new Color(GetAbsoluteColorChunk(difference.r , duration.TotalMilliseconds),
-                        GetAbsoluteColorChunk(difference.g , duration.TotalMilliseconds), 
-                        GetAbsoluteColorChunk(difference.b , duration.TotalMilliseconds),
-                        GetAbsoluteColorChunk(difference.a , duration.TotalMilliseconds));
+        return new Color(GetAbsoluteColorChunk(difference.r, duration.TotalMilliseconds),
+                        GetAbsoluteColorChunk(difference.g, duration.TotalMilliseconds),
+                        GetAbsoluteColorChunk(difference.b, duration.TotalMilliseconds),
+                        GetAbsoluteColorChunk(difference.a, duration.TotalMilliseconds));
     }
 
     public float GetAbsoluteColorChunk(float value, double duration)
