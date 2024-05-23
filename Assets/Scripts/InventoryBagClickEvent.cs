98c9beddda926d68a43f9107cac246ff0610679b
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2024-05-23 14:51:52 +0300

    Fix for raycast on only 1 panel, and not other objects + AI enhancements + Ai voice + controller + events + animations!!!

--- Assets/Scripts/InventoryBagClickEvent.cs
+++ Assets/Scripts/InventoryBagClickEvent.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventoryBagClickEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class InventoryBagClickEvent : MonoBehaviour
 {
     [SerializeField] RectTransform parentPosition;
     [SerializeField] Camera uiCamera;
@@ -12,23 +13,9 @@ public class InventoryBagClickEvent : MonoBehaviour, IPointerEnterHandler, IPoin
     [SerializeField] GraphicRaycaster graphicRayCaster;
     [SerializeField] EventSystem eventSystem;
     private PointerEventData m_pointerEventData;
+    private List<RaycastResult> m_rayCastResult = new List<RaycastResult>();
 
     private Vector2 m_positionInCanvas;
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        Debug.Log("CLICKING");
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        Debug.Log("ENTERING");
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        Debug.Log("EXITING");
-    }
-
     // Start is called before the first frame update
     void Start()
     {
@@ -36,13 +23,27 @@ public class InventoryBagClickEvent : MonoBehaviour, IPointerEnterHandler, IPoin
     }
 
    
-    void Update()
+    async void Update()
     {
         RectTransformUtility.ScreenPointToLocalPointInRectangle(parentPosition, Input.mousePosition, uiCamera, out m_positionInCanvas);
 
         m_pointerEventData.position = m_positionInCanvas; //canvas position
 
-        Debug.Log(m_positionInCanvas);
+        graphicRayCaster.Raycast(m_pointerEventData, m_rayCastResult);
 
+        Debug.Log(m_rayCastResult.Count);
+
+       // await IterateOverRayCasts(m_rayCastResult);
+
+
+    }
+
+    private async Task IterateOverRayCasts(List<RaycastResult> raycastResult)
+    {
+        foreach(var raycast in raycastResult)
+        {
+            Debug.Log(raycast);
+            await Task.Delay(0);
+        }
     }
 }
