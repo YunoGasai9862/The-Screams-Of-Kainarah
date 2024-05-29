b28d82dc28bdb459e841c2ff14a1e5c29c3360e0
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2024-05-29 14:19:19 +0300

    Crystal Moving Enhancments + Take a look at Bag Click event tomorrow + Ai images + voices + Controllers + health animations + more!!

--- Assets/Scripts/MoveCrystal.cs
+++ Assets/Scripts/MoveCrystal.cs
@@ -6,37 +6,55 @@ using UnityEngine;
 public class MoveCrystal : MonoBehaviour
 {
     private const string DIAMONG_TAG = "Diamond";
-    private const float DELAY_DURATION = 5F;
+    private const float DELAY_DURATION = 3f;
     private const float X_DEGREES = 45f;
     private const float ERROR_TERM = 1f;
+    private const float BOUNDS_ERROR_TERM = 5f;
 
     // Start is called before the first frame update
     private RectTransform m_diamondUILocation;
     private static bool m_increaseValue = false;
     private CancellationTokenSource m_cancellationTokenSource;   
     private CancellationToken m_cancellationToken;
+    private float m_heightOfCamera;
+    private float m_WidthOfCamera;
+    private Vector3 m_initialPosition;
+    private SpriteRenderer m_spriteRenderer;
     //Movement fix
     [SerializeField]
     public CrystalCollideEvent crystalCollideEvent;
 
     public static bool IncreaseValue { get => m_increaseValue; set => m_increaseValue = value; }
     public bool CrystalIsMoving { get; set; } = false;
+
+    public bool OutsideScreenBounds { get; set; } = false;
     public 
     void Start()
     {
         m_cancellationTokenSource = new CancellationTokenSource();
         m_cancellationToken = m_cancellationTokenSource.Token;
         m_diamondUILocation = GameObject.FindWithTag(DIAMONG_TAG).GetComponent<RectTransform>();
-
+        m_initialPosition = transform.position;
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
         crystalCollideEvent.AddListener(CrystalCollideListener);
+
+        m_heightOfCamera = 2f * Camera.main.orthographicSize;
+        //aspect ratio = width / height
+        m_WidthOfCamera = m_heightOfCamera * Camera.main.aspect;
+
     }
 
     private async void Update()
     {
-        //Get Diamond location on update - it changes
        
         if (CrystalIsMoving)
         {
+            if (await IsOutsideCameraBounds(transform.position, m_initialPosition, m_WidthOfCamera, m_heightOfCamera) && !OutsideScreenBounds)
+            {
+                m_spriteRenderer.color = new Color(m_spriteRenderer.color.r, m_spriteRenderer.color.g, m_spriteRenderer.color.b, 0);
+                OutsideScreenBounds = true;
+            }
+
             if (await IsCrystalAtTheGuiPanel(m_diamondUILocation.position))
             {
                 IncreaseValue = true;
@@ -44,14 +62,19 @@ public class MoveCrystal : MonoBehaviour
                 Destroy(gameObject);
             }
         }
-
     }
 
-    public Task<bool> IsCrystalAtTheGuiPanel(Vector3 updatedCrystalUIPosition)
+    private Task<bool> IsOutsideCameraBounds(Vector3 currentPos, Vector3 initialPos, float cameraWidth, float cameraHeight)
+    {
+        return Task.FromResult((currentPos.x  >= initialPos.x - BOUNDS_ERROR_TERM + cameraWidth / 2) && (currentPos.y >= initialPos.y - BOUNDS_ERROR_TERM + cameraHeight / 2));
+    }
+
+    private Task<bool> IsCrystalAtTheGuiPanel(Vector3 updatedCrystalUIPosition)
     {
         float difference = updatedCrystalUIPosition.x - updatedCrystalUIPosition.x * Mathf.Sin(X_DEGREES);
         return Task.FromResult(transform.position.x + difference + ERROR_TERM >= updatedCrystalUIPosition.x);
     }
+
 
     public void CrystalCollideListener(Collider2D collider, bool didCollide)
     {
