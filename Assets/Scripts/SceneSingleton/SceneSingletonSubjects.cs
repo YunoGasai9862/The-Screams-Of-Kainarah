d9bc8dbddb70f565d1d36e5262e06242638b6bcd
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2025-01-05 16:29:10 +0300

    Small comment!!

--- Assets/Scripts/SceneSingleton/SceneSingletonSubjects.cs
+++ Assets/Scripts/SceneSingleton/SceneSingletonSubjects.cs
@@ -1,5 +1,13 @@
 public class SceneSingletonSubjects
 {
+    //the solution i came to my mind - the better one
+    //as soon as the object enables it, instead of using these subjects,
+    //the object will call the delegator (the main listener), which will ping the actual object,
+    //the actual object can return/invoke anything via the the same delegator!!
+    //no need to use subjects - just on the onenable/ondisable, call it
+    //we dont want to continuously invoke that for all the observers, do we? 
+
+    //or we are using the subjects, then only ping back that particular subject, instead of all of them!!!
     private SubjectsToBeNotified<SceneSingleton> sceneSingletonSubjects = new SubjectsToBeNotified<SceneSingleton>();
 
     public SubjectsToBeNotified<SceneSingleton> SingletonSubjects { get => sceneSingletonSubjects; }
