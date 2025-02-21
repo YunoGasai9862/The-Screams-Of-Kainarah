e13d3e2507a7e7a53afa3776fb2fc8063c4b9405
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2025-02-21 16:28:21 +0300

    Adding params too my approach

--- Assets/Scripts/Interfaces/IDelegator.cs
+++ Assets/Scripts/Interfaces/IDelegator.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 public interface IDelegator<T>
 {
-    public IEnumerator NotifyObserver(IObserver<T> observer, T value, NotificationContext notificationContext = null);
-    public IEnumerator NotifySubject(IObserver<T> observer, NotificationContext notificationContext = null);
+    public IEnumerator NotifyObserver(IObserver<T> observer, T value, NotificationContext notificationContext = null, params object[] optional);
+    public IEnumerator NotifySubject(IObserver<T> observer, NotificationContext notificationContext = null, params object[] optional);
 }
--- Assets/Scripts/ObserverPattern/BaseDelegator.cs
+++ Assets/Scripts/ObserverPattern/BaseDelegator.cs
@@ -5,14 +5,14 @@ public abstract class BaseDelegator<T> : MonoBehaviour, IDelegator<T>
 {
     public Subject<IObserver<T>> Subject { get; set; }
 
-    public IEnumerator NotifyObserver(IObserver<T> observer, T value, NotificationContext notificationContext = null)
+    public IEnumerator NotifyObserver(IObserver<T> observer, T value, NotificationContext notificationContext = null, params object[] optional)
     {
         observer.OnNotify(value);
 
         yield return null;
     }
 
-    public IEnumerator NotifySubject(IObserver<T> observer, NotificationContext notificationContext = null)
+    public IEnumerator NotifySubject(IObserver<T> observer, NotificationContext notificationContext = null, params object[] optional)
     {
         yield return new WaitUntil(() => Subject.GetSubject() != null);
 
