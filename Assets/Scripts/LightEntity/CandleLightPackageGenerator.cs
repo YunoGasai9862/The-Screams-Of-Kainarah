2b9cee846c3f2ededf63758fe5a9f2e3b0b09cb4
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2025-04-10 10:27:25 +0300

    Start working on the light generation logic now all set + AI voices + images + finish game soon!!!

--- Assets/Scripts/LightEntity/CandleLightPackageGenerator.cs
+++ Assets/Scripts/LightEntity/CandleLightPackageGenerator.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections;
 using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 [ObserverSystem(SubjectType = typeof(LightFlicker), ObserverType = typeof(CandleLightPackageGenerator))]
 [ObserverSystem(SubjectType = typeof(CandleLightPackageGenerator), ObserverType = typeof(CustomLightProcessing))]
@@ -11,12 +15,20 @@ public class CandleLightPackageGenerator : MonoBehaviour, ISubject<IObserver<Lig
     LightPreprocessDelegator lightPreprocessDelegator;
 
 
-    private ILightPreprocess lightFlickerPreprocess;
+    private ILightPreprocess LightPreprocess { get; set; }
 
     private string LightFlickerUniqueKey { get; set; }
 
+    private bool CustomPreprocessingScriptIsAlive { get; set;}
+
+    private Light2D LightSource { get; set; }
+
     private void Start()
     {
+        LightSource = GetComponent<Light2D>();
+
+        ValidateLightSourcePresence(LightSource);
+
         StartCoroutine(lightPreprocessDelegator.NotifySubject(this, new NotificationContext()
         {
             ObserverName = gameObject.name,
@@ -29,22 +41,47 @@ public class CandleLightPackageGenerator : MonoBehaviour, ISubject<IObserver<Lig
         lightPackageDelegator.GetSubject(gameObject.tag).SetSubject(this);
     }
 
-    private bool CalculateDistance()
+    //start calculating distance recursively - however with the aid of the player
+    private IEnumerator CalculateDistance(LightPackage lightPackage)
     {
-        return false;
+        yield return null;
     }
 
     private LightPackage PrepareLightPackage()
     {
-        return null;
+        return new LightPackage()
+        {
+            LightPreprocess = LightPreprocess,
+            LightSource = LightSource,
+            LightProperties = new LightProperties() // fill this out
+        };
+    }
+
+    private IEnumerator PrepareDataForCustomLightningGeneration()
+    {
+        yield return new WaitUntil(() => lightPreprocessDelegator != null);
+
+        //first prepare the initial light data
+        LightPackage lightPackage = PrepareLightPackage();
+
+        //then start the coroutine for Calculating Distance - this will be linked with Player's data
     }
 
     public void OnNotify(ILightPreprocess data, NotificationContext context, SemaphoreSlim semaphoreSlim, params object[] optional)
     {
-        lightFlickerPreprocess = data;
+        LightPreprocess = data;
     }
 
     public void OnNotifySubject(IObserver<LightPackage> data, NotificationContext notificationContext, params object[] optional)
     {
+        StartCoroutine(PrepareDataForCustomLightningGeneration());
+    }
+
+    private void ValidateLightSourcePresence(Light2D light2D)
+    {
+        if (light2D == null)
+        {
+            throw new ApplicationException("LightSource is not Present!");
+        }
     }
 }
