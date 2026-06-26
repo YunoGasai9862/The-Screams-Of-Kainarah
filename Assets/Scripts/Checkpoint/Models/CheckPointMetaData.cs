5047a8f5e11702fd94e7d8daa427b94f1a6b7d09
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2026-06-27 00:23:20 +0500

    WIP (checkpoint infra)

---
+++ Assets/Scripts/Checkpoint/Models/CheckPointMetaData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assets.Scripts.Checkpoint.Models
+{
+    public class CheckPointMetaData
+    {
+        public Guid Uuid { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public string Location { get; set; }
+
+        public String SceneName { get; set; }
+
+        public string SceneVersion { get; set; }
+
+        public List<string> RegisteredObjects { get; set; } = new List<string>();
+
+        public CheckPointMetaData()
+        {
+            Uuid = Guid.NewGuid();
+            TimeStamp = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Assets/Scripts/Checkpoint/Models/CheckpointModelView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assets.Scripts.Checkpoint.Models
+{
+    public class CheckpointModelView
+    {
+        public int Row { get; set; }
+        CheckPointMetaData CheckPointMetaData { get; set; }
+    }
+}
